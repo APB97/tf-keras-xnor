19ffbce8ff2a4ef37c037a3fe9f8c980656928b0
Adrian Bieliński <adrian.bielinski.pl@gmail.com>
2021-09-21 15:58:35 +0200

    Updated main.py, adding commandline arguments

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
+from argparse import ArgumentParser
+
 import numpy
 from keras.models import Sequential
 from keras.layers import Dense
 
-if __name__ == '__main__':
-    dataset1 = numpy.loadtxt('xnor.csv', delimiter=',')
-    A = numpy.array(dataset1[:, 0:2], 'uint8')
-    B = numpy.array(dataset1[:, 2:3], 'uint8')
 
-    print(A)
-    print(B)
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('--dataset', help='csv file with inputs and desired outputs')
+    parser.add_argument('--data_delimiter', help='delimiter in dataset csv file')
+    args = parser.parse_args()
+    inputs, desired_output = load_dataset(args.dataset, args.data_delimiter)
 
+    print(inputs)
+    print(desired_output)
     model = Sequential()
     model.add(Dense(2, activation='elu', input_dim=2))
     model.add(Dense(4, activation='elu'))
     model.add(Dense(1, activation='sigmoid'))
-
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    model.fit(A, B, epochs=1000, verbose=0)
+    model.fit(inputs, desired_output, epochs=1000, verbose=0)
+    _, accuracy = model.evaluate(inputs, desired_output)
+    print(f'Accuracy: {accuracy * 100}')
+    predictions = model.predict(inputs)
+    for i in range(len(inputs)):
+        print(f'{inputs[i].tolist()} => {round(predictions[i][0])} (expected: {desired_output[i]})')
+    model.save('model/XNOR.h5')
 
-    _, accuracy = model.evaluate(A, B)
-    print(f'Accuracy: {accuracy*100}')
 
-    predictions = model.predict_classes(A)
-    for i in range(len(A)):
-        print(f'{A[i].tolist()} => {predictions[i]} (expected: {B[i]})')
+def load_dataset(dataset_file, delimiter):
+    dataset1 = numpy.loadtxt(dataset_file, delimiter=delimiter)
+    inputs = numpy.array(dataset1[:, 0:2], 'uint8')
+    desired_output = numpy.array(dataset1[:, 2:3], 'uint8')
+    return inputs, desired_output
 
-    model.save('XNOR.model')
+
+if __name__ == '__main__':
+    main()
